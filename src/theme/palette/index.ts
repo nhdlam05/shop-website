1de0b9a3eed4a6a78bc6cbbb21308df05e4e0896
nhdlam05 <101357677+nhdlam05@users.noreply.github.com>
2024-03-31 17:22:02 +0700

    Task/update theme (#1)
    * ✨ Update theme for light and dark mode for project
    
    * 🚧
    
    * 🚧
    
    * 🚧

--- src/theme/palette/index.ts
+++ src/theme/palette/index.ts
@@ -16,8 +16,8 @@ const DefaultPalette = (mode: Palette['mode'], skin: Skin): Palette => {
     } else if (skin === 'bordered' && mode === 'dark') {
       return darkPaperBgColor
     } else if (mode === 'light') {
-      return '#F8F7FA'
-    } else return '#25293C'
+      return '#EEEEEE'
+    } else return '#222831'
   }
 
   return {
